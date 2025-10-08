e4e7ac3c00c49e72020dfeb5cb8b56b4a413d82b
Ankush Choubey <ankushchoubey@outlook.com>
2025-10-08 14:50:24 +0530

    feat: project setup

---
+++ src/main/kotlin/com/ankush/fullstacktest/FullstacktestApplication.kt
@@ -0,0 +1,11 @@
+package com.ankush.fullstacktest
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FullstacktestApplication
+
+fun main(args: Array<String>) {
+	runApplication<FullstacktestApplication>(*args)
+}
